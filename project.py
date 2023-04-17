c7059a539f7172d7a93c72a7085155bd0d9c32fd
Mohit Tandon <mohit.tandon@hotmail.com>
2023-04-17 13:13:58 +0530

    Update project.py

--- project.py
+++ project.py
@@ -6,16 +6,15 @@ service = Service()
 options= Options()
 driver_service = Service(executable_path="/usr/bin/chromedriver")
 
-
 class TestWebsiteLoad(unittest.TestCase):
  
 
     # Define a method to test website loading
     def test_load_website(self):
-        options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(service=driver_service, options=options) # Use any browser driver of your choice.
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(service=driver_service, options=options) # Use any browser driver of your choice.
         driver.get("https://atg.world/")
         print(driver.title)
-        driver.quit()
+        driver.quit()
